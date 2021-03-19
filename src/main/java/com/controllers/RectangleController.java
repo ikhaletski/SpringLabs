0e28e94a3c2d4e31bdfaf9f43472f3995ab42abc
Ilya Khaletski <il.khaletski@gmail.com>
2021-03-19 13:36:20 +0300

    Added Interface Calculator Fixed pom.xml

--- src/main/java/com/controllers/RectangleController.java
+++ src/main/java/com/controllers/RectangleController.java
@@ -1,23 +1,22 @@
 package com.controllers;
 
-import com.model.Calculator;
+import com.model.RectangleCalculator;
 import com.model.Rectangle;
 import com.model.ResultsOfCalculations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class RectangleController {
 
-    private Calculator calculator;
+    private RectangleCalculator calculator;
     private Rectangle rectangle;
 
     @GetMapping("/Calculate")
-    public ResultsOfCalculations getParameters(@RequestParam(name = "length") float length,
-                                               @RequestParam(name = "width") float width) {
-        calculator = new Calculator();
+    public ResultsOfCalculations getParameters(@RequestParam() float length,
+                                               @RequestParam() float width) {
+        calculator = new RectangleCalculator();
         rectangle = new Rectangle(width, length);
         return calculator.calculate(rectangle);
     }
--- src/main/java/com/model/Calculator.java
+++ src/main/java/com/model/Calculator.java
@@ -1,12 +1,6 @@
 package com.model;
 
-public class Calculator {
+public interface Calculator {
 
-    public ResultsOfCalculations calculate(Rectangle rectangle) {
-        ResultsOfCalculations resultsOfCalculations = new ResultsOfCalculations(
-                rectangle.getLength() * rectangle.getWidth(),
-                (rectangle.getLength() + rectangle.getWidth()) * 2
-        );
-        return resultsOfCalculations;
-    }
+    ResultsOfCalculations calculate(Rectangle rectangle);
 }
